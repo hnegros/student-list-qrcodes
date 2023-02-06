b8bba92d7847fb6e69f2b0c919fd4a44abcf8ca2
hnegros <hadrien.negros@gmail.com>
2023-02-06 21:41:45 +0000

    fichier reel

--- generate_qrcodes.py
+++ generate_qrcodes.py
@@ -8,38 +8,37 @@ version=3
 
 border=2
 box_size=14
-font_size=40
-
-os.makedirs("qrcodes",exist_ok=True)
+font_size=25
 
 def get_nb_module_version(n):
     return 21+(n-1)*4
 
+for filename in os.listdir("input"):
+    with open("input/"+filename) as csvfile:
+        classe = filename.replace(".csv","")
+        os.makedirs(f"qrcodes/{classe}",exist_ok=True)
+        reader = csv.reader(csvfile,delimiter=";",quotechar="\"")
+        for row in reader:
+            qr = qrcode.QRCode(
+                border=border,
+                version=version,
+                box_size=box_size
+            )
+            qr.add_data(row[0]+";"+classe)
+            qr.make(fit=True)
 
-with open('fake_student_list.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        qr = qrcode.QRCode(
-            border=border,
-            version=version,
-            box_size=box_size
-        )
-        qr.add_data(";".join(row))
-        qr.make(fit=True)
+            size = (get_nb_module_version(qr.version)+(border*2))*box_size
+            H,W = size,size
 
-        size = (get_nb_module_version(qr.version)+(border*2))*box_size
-        H,W = size,size
+            img = qr.make_image()
 
-        img = qr.make_image()
+            background = Image.new('RGBA', (H, W+2*(font_size+4) + 2*box_size), (255,255,255,255))
+            draw = ImageDraw.Draw(background)
 
-        background = Image.new('RGBA', (H, W+3*(font_size+4) + 2*box_size), (255,255,255,255))
-        draw = ImageDraw.Draw(background)
+            font = ImageFont.truetype("Roboto-Regular.ttf",font_size)
+            draw.text((H/2,W+(font_size/2)),row[0],(0,0,0),anchor="mm", font=font)
+            draw.text((H/2,W+(font_size/2) + font_size+4),classe,(0,0,0),anchor="mm", font=font)
+            background.paste(img, (0,0))
+            background.save(f'qrcodes/{classe}/{row[0]+"_"+classe}.png')
 
-        font = ImageFont.truetype("Roboto-Regular.ttf",font_size)
-        draw.text((H/2,W+(font_size/2)),row[0],(0,0,0),anchor="mm", font=font)
-        draw.text((H/2,W+(font_size/2) + font_size+4),row[1],(0,0,0),anchor="mm", font=font)
-        draw.text((H/2,W+(font_size/2)+ (font_size+4) *2),row[2],(0,0,0),anchor="mm", font=font)
-        background.paste(img, (0,0))
-        background.save(f'qrcodes/{"_".join(row)}.png')
-
- 
+    
